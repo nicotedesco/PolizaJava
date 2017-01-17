e960b7a17e16cb020faa9143f0c7f31c54fd0f23
a053629318b <a053629318b@s09-pc12>
2017-01-17 12:01:14 +0100

    Hola

---
+++ src/poliza/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliza;
+
+/**
+ *
+ * @author a053629318b
+ */
+public class Persona {
+    String nombre, direccion, dni;
+    int edad;
+    
+    
+}
---
+++ src/poliza/Poliza.java
@@ -0,0 +1,17 @@
+
+package poliza;
+
+/**
+ *
+ * @author a053629318b
+ */
+public class Poliza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
